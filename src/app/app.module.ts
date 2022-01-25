cafdf28c9e394371e165a7409b8550b0cf904b49
tanish0701 <tanish.singh@hatch.com>
2022-01-25 23:21:49 +0530

    changes 25-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { PracticeComponent } from './practice/practice.component';
+import { WarningAlertComponent } from './warningalert/warning-alert.component';
+import { SuccessAlertComponent } from './sucess-alert/success-alert.components';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    PracticeComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ 
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
